9f52d6d54db6c5786b7cdd46864d6084c68ddde2
Junhan Hu <junhanh@gmail.com>
2016-12-01 23:39:03 -0600

    Add files via upload

---
+++ lr_sklearn.py
@@ -0,0 +1,25 @@
+import numpy as np
+import util as util
+from sklearn.linear_model import LogisticRegression
+
+data, labels = util.getDataAndLabels()
+data = util.PCAreduce(data)
+num_features = len(data[0])
+
+labels = np.argmax(labels, 1)
+
+labels_size = len(labels)
+valid_size = 2000
+test_start_index = labels_size -  valid_size
+
+train_dataset = data[:test_start_index]
+train_labels = labels[:test_start_index]
+
+test_dataset = data[test_start_index:]
+test_labels = labels[test_start_index:]
+
+model = LogisticRegression()
+model = model.fit(train_dataset, train_labels)
+
+print(model.score(train_dataset, train_labels))
+print(model.score(test_dataset, test_labels))
